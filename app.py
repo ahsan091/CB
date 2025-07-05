cf3f556543a51e8f505ec9e9788f99e7d43dc962
Ahsan Abdullah <106489956+ahsan091@users.noreply.github.com>
2025-07-06 00:03:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, Response
 
 app = Flask(__name__)
 
@@ -12,7 +12,9 @@ def robots():
 
 @app.route('/sitemap.xml')
 def sitemap():
-    return send_from_directory('static', 'sitemap.xml')
+    with open('static/sitemap.xml') as f:
+        sitemap_content = f.read()
+    return Response(sitemap_content, mimetype='application/xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
