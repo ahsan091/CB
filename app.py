4681d441704824151fb757cdcbb926600cff276e
Ahsan Abdullah <106489956+ahsan091@users.noreply.github.com>
2025-07-06 16:22:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, Response
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -6,15 +6,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/insights')
+def insights():
+    return render_template('insights.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/robots.txt')
 def robots():
     return send_from_directory('static', 'robots.txt')
 
 @app.route('/sitemap.xml')
 def sitemap():
-    with open('static/sitemap.xml') as f:
-        sitemap_content = f.read()
-    return Response(sitemap_content, mimetype='application/xml')
+    return send_from_directory('static', 'sitemap.xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
